951d336c5295b02746b2791b1cf3614623973a92
Brett <bcrafton@gmail.com>
2022-10-16 11:03:44 -0500

    fix- F-E bug Date refresh

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+let date = new Date()
+let selectedDate = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate()
+
+const dateInput = document.querySelector('#appointmentDate')
+const doctorList = [...document.querySelectorAll('.doctorsForm')]
+
+dateInput.addEventListener('change', setDate)
+
+function setDate(){
+    selectedDate = dateInput.value
+    console.log(selectedDate)
+
+    updateDoctorsAction()
+}
+
+function updateDoctorsAction(){
+    doctorList.forEach(doctor => doctor.action = doctor.action.slice(0, -22) + selectedDate + "?_method=GET")
+}
+updateDoctorsAction()
