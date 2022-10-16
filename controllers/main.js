1c0932ba092ff5a3307289bd35ea164da8365019
Brett <bcrafton@gmail.com>
2022-10-16 10:54:09 -0500

    conditionals added 15m and 3 appt max

--- controllers/main.js
+++ controllers/main.js
@@ -4,8 +4,10 @@ const Appointment = require("../models/Appointment")
 module.exports = {
   getIndex: async (req, res) => {
     try{
+      let date = new Date()
+      let todaysDate = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate()
       const doctors = await Doctor.find()
-      let appointments = []
+      let appointments = await Appointment.find({date: todaysDate})
       res.render("index.ejs", {doctors: doctors, appointments: appointments})
     } catch (err) {
     console.log(err)
@@ -44,6 +46,21 @@ module.exports = {
   },
   newAppointment: async (req, res) =>{
     try{
+      //check DB to see how many appointments are at requested time/date for doctor
+      const currentTimeAppointments = await Appointment.find({doctorId: JSON.parse(req.body.doctor).id, date: req.body.date, time: req.body.time })
+
+      //if selected time is not a 15min increment, throw error (step attribute in "Time Input" in EJS front-end already stops this, but this stops other appointment injections / other front-ends)
+      if(+(req.body.time.split(":")[1]) % 15){
+        throw Error("Appointment must be scheduled in 15min increments")
+      }
+
+      //if more than 3 appointments already scheduled, throw error
+      else if(currentTimeAppointments.length >= 3){
+        throw Error("Doctor booked at this time. Try a different time or different doctor.")
+      }
+
+      //else create appointment
+      else{
         await Appointment.create({
           firstName: req.body.firstName,
           lastName: req.body.lastName,
@@ -54,6 +71,7 @@ module.exports = {
           doctorId: JSON.parse(req.body.doctor).id
         })
         console.log("Appointment has been added!")
+      }
       res.redirect('back')
       
     } catch (err) {
